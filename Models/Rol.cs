32e2d007f92b37ae6eacd16fb2ff6d3fae3832df
Hiro-DP <deyanira.paiba@es.bicu.edu.ni>
2025-11-23 15:58:36 -0600

    Prueba 2

--- Models/Rol.cs
+++ Models/Rol.cs
@@ -5,6 +5,8 @@ namespace Sistema_Web_de_Nominas.Models
     [Table("Rol")]
     public class Rol
     {
+        //Prueba 2
+
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public ICollection<Usuario> Usuario { get; set; }
