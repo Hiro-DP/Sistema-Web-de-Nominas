44494c0f59c123348671bbcb416105d27db680b1
Hiro-DP <deyanira.paiba@es.bicu.edu.ni>
2025-11-25 15:52:13 -0600

    Migracion de la base de datos completa

--- Migrations/20251125003634_Subida2.Designer.cs
+++ Migrations/20251125003634_Subida2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sistema_Web_de_Nominas.Data;
 
@@ -11,9 +12,11 @@ using Sistema_Web_de_Nominas.Data;
 namespace Sistema_Web_de_Nominas.Migrations
 {
     [DbContext(typeof(NominaDbContext))]
-    partial class NominaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251125003634_Subida2")]
+    partial class Subida2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251125003634_Subida2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema_Web_de_Nominas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Subida2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Empleado",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleado_UsuarioId",
+                table: "Empleado",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empleado_Usuario_UsuarioId",
+                table: "Empleado",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empleado_Usuario_UsuarioId",
+                table: "Empleado");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Empleado_UsuarioId",
+                table: "Empleado");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Empleado");
+        }
+    }
+}
